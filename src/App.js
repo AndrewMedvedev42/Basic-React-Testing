d7b4993edba81f2b132371f3de5204e295711831
andrewNEW42 <sanjino56@gmail.com>
2021-09-20 19:37:37 +0300

    added App.Test and Checkbox.Test

--- src/App.js
+++ src/App.js
@@ -1,28 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-
-// https://api.github.com/users/eveporcello
-
 function App() {
-  const [data, setData] = useState(null);
 
-  useEffect(() => {
-    fetch(`https://api.github.com/users/${login}`)
-      .then(response => response.json())
-      .then(setData);
-  }, []);
-
-  if (data) {
     return (
       <div>
-        <h1>{data.name}</h1>
-        <p>{data.location}</p>
-        <img alt={data.login} src={data.avatar_url} />
+        <h1>what</h1>
       </div>
     );
-  }
-
-  return <div>No Data Available</div>;
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Is there a h1?', () => {
+  const { getByText} = render(<App />);
+  const h1 = getByText(/what/);
+  expect(h1).toBeInTheDocument();
 });
